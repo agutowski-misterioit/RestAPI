7f12169d4d4ecc899767ae516c9ce7399d8687a2
Andrzej Gutowski <a.gutowski@misterioit.pl>
2024-12-22 10:42:06 +0100

    refresh gitignore

--- app/env.config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.api_port = exports.api_name = void 0;
-const dotenv = require("dotenv");
-dotenv.config({ debug: true, encoding: 'UTF-8' });
-exports.api_name = process.env.API_NAME;
-exports.api_port = process.env.API_PORT;
